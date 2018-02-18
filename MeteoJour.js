df4ae207d3a67e626a6817061240ab4f2d84a079
aTewan <tewan33@gmail.com>
2018-02-18 21:34:16 +0100

    Programmation objet

--- MeteoJour.js
+++ MeteoJour.js
@@ -1,3 +1,11 @@
+'use strict';
+
+const chalk = require('chalk');
+const request = require('request');
+const Table = require('cli-table2');
+
+const prev_jour_ville = "Prévisions météorologiques d'aujourd'hui pour la ville de : ";
+
 module.exports = class MeteoJour {
 
     constructor(json) {
@@ -9,4 +17,36 @@ module.exports = class MeteoJour {
         this.humidite = json.main.humidity;     
     }
 
+    static creerFromJson(json) {
+        let meteoJour = new MeteoJour(json);
+        return meteoJour;
+    }
+
+    static capitalize(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    static affichage(MeteoJour) {
+        console.log(prev_jour_ville + chalk.bold(MeteoJour.ville) + ", " + chalk.bold(MeteoJour.pays));
+
+        let description = MeteoJour.description;
+        let temperature;
+        if(MeteoJour.temp_min === MeteoJour.temp_max) {
+            temperature = MeteoJour.temp_min+"°C";
+        }
+        else {
+            temperature = MeteoJour.temp_min+"-"+MeteoJour.temp_max+"°C"
+        }
+        let humidite = chalk.cyan(MeteoJour.humidite +"%");
+        //let icone = valorisationIcone(description);
+
+        var table = new Table({ chars: {'mid': '', 'left-mid': '', 'mid-mid': '', 'right-mid': ''} });
+        table.push(
+            [description],
+            [temperature],
+            [humidite]
+        );
+
+        console.log(table.toString());
+    }
 }
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const MeteoJour = require('./MeteoJour');
 
 /** Clé pour appeler l'API OpenWeatherAPI */
 const key = "3130f4dad1d25281d3c4a7bfb4363f37";
-const prev_jour_ville = "Prévisions météorologiques d'aujourd'hui pour la ville de : ";
+
 
 
 /** Configuration pour commander */
@@ -22,53 +22,30 @@ program
 /** Option possible pour commander */
 if(program.day) {
     let url = "http://api.openweathermap.org/data/2.5/weather?q=" + program.day +  "&units=metric&lang=fr&appid=" + key;
-    getMeteoVilleJour(url);
+    getMeteoVilleJour(url).then(function(body) {
+        MeteoJour.affichage(body);
+    })
+    .catch(function(err) {
+        console.log("Erreur : " +err)
+    });
+
 }
 
-/**
- * getMeteoVilleJour
- * @param {*} url 
- * Cette fonction permet de récupérer la météo du jour d'une ville
- */
 function getMeteoVilleJour(url) {
-    request.get(url, (error, response, body) =>  {
-        let json = JSON.parse(body);
-        let meteo = new MeteoJour(json);    
-        affichage(meteo);
-    });
+    return new Promise((resolve, reject) => {
+        request.get(url, (err, res, body) =>  {
+            if(err){
+                reject(err); return;
+            }
+            let meteoJour = MeteoJour.creerFromJson(JSON.parse(body));
+            resolve(meteoJour);
+            });
+    })
+    
 }
 
 function title() {
     asciify('Meteo', {font: 'larry3d'}, function(err, res){ console.log(chalk.bgCyan(res)) });
-}
-
-String.prototype.capitalize = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1);
-}
-
-function affichageTemperature(min,max) {
-    if(min === max)
-        return(min+"°C");
-    else
-        return(min+"-"+max+"°C");
-}
-
-function affichage(MeteoJour) {
-    console.log(prev_jour_ville + chalk.bold(MeteoJour.ville) + ", " + chalk.bold(MeteoJour.pays));
-
-    let description = MeteoJour.description.capitalize();
-    let temperature = affichageTemperature(MeteoJour.temp_min, MeteoJour.temp_max);
-    let humidite = chalk.cyan(MeteoJour.humidite +"%");
-
-    let icone = valorisationIcone(description);
-    var table = new Table({ chars: {'mid': '', 'left-mid': '', 'mid-mid': '', 'right-mid': ''} });
-    table.push(
-        [icone[0], description],
-        [icone[1], temperature],
-        [icone[2], humidite]
-    );
-
-    console.log(table.toString());
 }
 
 function valorisationIcone(description) {
