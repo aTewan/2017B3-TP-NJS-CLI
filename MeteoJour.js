4751fbbda1fefe9a6ffa1b69501e133d8822a0ea
aTewan <tewan33@gmail.com>
2018-02-20 09:17:57 +0100

    Ajout de documentation

--- MeteoJour.js
+++ MeteoJour.js
@@ -12,6 +12,11 @@ const prev_jour_ville = "Prévisions météorologiques d'aujourd'hui pour la vil
 
 module.exports = class MeteoJour {
 
+    /**
+     * constructor
+     * @param {*} json
+     * Constructeur de MeteoJour. 
+     */
     constructor(json) {
         this.date = moment().format('L');
         this.ville = json.name;
@@ -22,10 +27,21 @@ module.exports = class MeteoJour {
         this.humidite = json.main.humidity;     
     }
 
+    /**
+     * creerFromJson
+     * @param {*} json 
+     * Fonction qui créer un objet MeteoJour avec un json en paramètre.
+     */
     static creerFromJson(json) {
         let meteoJour = new MeteoJour(json);
         return meteoJour;
     }
+
+    /**
+     * affichage
+     * @param {*} MeteoJour
+     * Fonction qui permet d'afficher la météo.
+     */
 
     static affichage(MeteoJour) {
         console.log(prev_jour_ville + chalk.bold(MeteoJour.ville) + ", " + chalk.bold(MeteoJour.pays));
@@ -33,6 +49,11 @@ module.exports = class MeteoJour {
         let description = MeteoJour.description;
         description = description.replace(/(^|\s)\S/g, l => l.toUpperCase())
         let temperature;
+
+        /** Si la température minimum est égale à la température maximum
+         *  alors on affiche qu'une seule fois la température
+         *  sinon on affiche la température minimum est maximum */
+
         if(MeteoJour.temp_min === MeteoJour.temp_max) {
             temperature = MeteoJour.temp_min+"°C";
         }
